6561d3261c25e4e1b2760108d5d14938828a5d16
Graham Cottridge <codinggc@gmail.com>
2020-05-26 23:57:03 +0100

    Combine reducers in app

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider, connect } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import { createLogger } from "redux-logger";
 import thunkMiddleware from "redux-thunk";
 import "./index.css";
 import App from "./containers/App";
 import registerServiceWorker from "./registerServiceWorker";
-import { searchRobots } from "./reducers";
+import { searchRobots, requestRobots } from "./reducers";
 import "tachyons";
 
 const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestRobots });
 const store = createStore(
-  searchRobots,
+  rootReducer,
   applyMiddleware(thunkMiddleware, logger)
 );
 
